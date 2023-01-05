3a4894c03df74fd7e9ca97daeb9c008cea8f478e
Fernando Maceda <fernando.patricio.maceda@gmail.com>
2023-01-05 15:32:11 -0600

    Update db_task.sql

--- To Do List App/database/db_task.sql
+++ To Do List App/database/db_task.sql
@@ -1,32 +1,10 @@
--- phpMyAdmin SQL Dump
--- version 4.7.0
--- https://www.phpmyadmin.net/
---
--- Host: 127.0.0.1
--- Generation Time: Jun 01, 2018 at 07:22 AM
--- Server version: 10.1.24-MariaDB
--- PHP Version: 7.1.6
-
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
 START TRANSACTION;
 SET time_zone = "+00:00";
 
 
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-
---
--- Database: `db_task`
---
-
--- --------------------------------------------------------
-
---
--- Table structure for table `task`
---
+USE db_task;
 
 CREATE TABLE `task` (
   `task_id` int(11) NOT NULL,
@@ -62,7 +40,3 @@ ALTER TABLE `task`
 --
 ALTER TABLE `task`
   MODIFY `task_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;COMMIT;
-
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
